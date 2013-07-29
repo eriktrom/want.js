2e459e8e266bf6d08692a0041e00791ed6fa93b8
Erik Trom <erik.trom@gmail.com>
2013-07-28 18:35:27 -1000

    add basic defer function

---
+++ test/unit/defer_test.js
@@ -0,0 +1,30 @@
+import { defer } from 'want/defer';
+
+
+module("defer");
+
+test("Returns an object with a 'resolve' method", function() {
+  expect(1);
+  ok(typeof defer().resolve === "function");
+});
+
+test("Returns an object with a 'then' method", function() {
+  expect(1);
+  ok(typeof defer().then === "function");
+});
+
+asyncTest("Registers an observer(callback) that is notified(called) and returns " +
+          "a value equal to 1 in the next turn", function() {
+  expect(1);
+
+  function eventuallyReturnOne () {
+    var deferred = defer();
+    deferred.resolve(1);
+    return deferred;
+  }
+
+  eventuallyReturnOne().then(function(value) {
+    start();
+    equal(value, 1);
+  });
+});
