4bcafd81f5cc2853a5f7dec797f7ea6059dcc62f
Erik Trom <erik.trom@gmail.com>
2013-07-28 19:48:55 -1000

    principle of least authority, seperate resolver from promise(observer of resolution)

--- test/unit/defer_test.js
+++ test/unit/defer_test.js
@@ -3,14 +3,19 @@ import { defer } from 'want/defer';
 
 module("defer");
 
-test("Returns an object with a 'resolve' method", function() {
+test("resolve - is a function that provides the capability to determine the resolution", function() {
   expect(1);
   ok(typeof defer().resolve === "function");
 });
 
-test("Returns an object with a 'then' method", function() {
-  expect(1);
-  ok(typeof defer().then === "function");
+test("promise & promise.then - promise is an object that gives the authority to " +
+     "observe the resolution and act accordingly via its 'then' method", function() {
+  expect(2);
+
+  var promise = defer().promise;
+
+  ok(typeof promise === "object");
+  ok(typeof promise.then === "function");
 });
 
 asyncTest("Registers an observer(callback) that is notified(called) and returns " +
@@ -20,7 +25,7 @@ asyncTest("Registers an observer(callback) that is notified(called) and returns 
   function eventuallyReturnOne () {
     var deferred = defer();
     deferred.resolve(1);
-    return deferred;
+    return deferred.promise;
   }
 
   eventuallyReturnOne().then(function(value) {
@@ -36,7 +41,7 @@ asyncTest("Only the first call to resolve can set the resolution", function() {
     var deferred = defer();
     deferred.resolve(1);
     deferred.resolve(2);
-    return deferred;
+    return deferred.promise;
   }
 
   eventuallyReturnOne().then(function(value) {
