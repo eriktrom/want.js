3d072b995c2c71c6a17ebb073732fb099b3cc4b1
Erik Trom <erik.trom@gmail.com>
2013-08-02 22:12:28 -1000

    update karma to 0.9

--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,77 @@
 // Karma configuration
-// Generated on Thu Jun 13 2013 18:52:40 GMT-1000 (HST)
+// Generated on Fri Aug 02 2013 21:19:16 GMT-1000 (HST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: 'tmp/public',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['qunit'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  QUNIT,
-  QUNIT_ADAPTER,
-  'tmp/public/vendor.js',
-  'tmp/public/tests.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'vendor.js',
+      'tests.js'
+    ],
 
 
-// list of files to exclude
-exclude = [
+    // list of files to exclude
+    exclude: [
 
-];
+    ],
 
+    plugins: [
+      'karma-qunit',
+      'karma-phantomjs-launcher',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-safari-launcher',
+      'karma-growl-reporter'
+    ],
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress', 'growl'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'growl'],
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // web server port
+    port: 9876,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['ChromeCanary'];
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
